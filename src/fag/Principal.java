dcc735ee27c2f1ce99b0fcbfc04f2e9a3081f875
luizmarinello <lhmdrosa@minha.fag.edu.br>
2025-09-15 10:38:31 -0300

    luizmarinello.java
    adicionei o meu nome

--- src/fag/Principal.java
+++ src/fag/Principal.java
@@ -4,7 +4,7 @@ public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+       system.out.println("sou o luiz henrique marinello da rosa")
 	}
 
 }
